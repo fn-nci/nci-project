9572593990259ded6497c1be2f2293dca59ddf6a
Flavio Nunes <flavio.nunes@mapall.com>
2025-03-14 13:08:26 +0000

    added CD - Deploy SH

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+# Deployment Process 
 #!/usr/bin/env bash
 sudo apt update && sudo apt install nodejs npm
 # Install pm2 which is a production process manager for Node. js with a built-in load balancer.
